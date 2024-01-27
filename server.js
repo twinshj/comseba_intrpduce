93792175968bd8813a1bd13ae60e3a1f74b9c383
twinshj <smallgiant0604@gmail.com>
2024-01-27 15:00:43 +0900

    마마마마마마마마

---
+++ server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import path from "path";
+import fs from "fs";
+const _dirname = path.resolve();
+const app = express();
+app.use(express.static("frontend"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const port = 3000;
+app.get("/", (req, res) => {
+  res.sendFile(_dirname + "/frontend/src/html/home.html");
+});
+app.get("/guest", (req, res) => {
+  res.sendFile(_dirname + "/frontend/src/html/guestBook.html");
+});
+app.get("/comment", (req, res) => {
+  const db = JSON.parse(fs.readFileSync("DB.json"));
+  res.json(db);
+});
+app.listen(port, () => {
+  console.log("server is listening at localhost: 3000");
+});
+app.post("/createcomment", (req, res) => {
+  const data = req.bady;
+  console.log(data);
+
+  const db = JSON.parse(fs.readFileSync("DB.json"));
+
+  db.commentList.unshift(data);
+  fs.writeFileSync("DB.json", JSON.stringify(db));
+  res.json(data);
+});
+app.get("/map", (rep, res) => {
+  res.sendFile(_dirname + "/frontend/src/html/map.html");
+});
